23888caabdb3e42ac4952f4dac1155c0204bdbe3
andrejabramov <andrzejvod@gmail.com>
2025-04-10 16:34:42 +0300

    hotels_order39

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,45 +1,25 @@
-from fastapi import APIRouter, HTTPException, Response
-from passlib.context import CryptContext
-import jwt
-from datetime import datetime, timezone, timedelta
+from fastapi import APIRouter, HTTPException, Response, Request, Header
+from fastapi.responses import JSONResponse
 
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserRequestAdd, UserAdd
 from src.database import async_session_maker
-
+from src.services.auth import AuthService
 
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def create_access_token(data: dict) -> str:
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode |= {"exp": expire}
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
 
 
 @router.post("/register")
 async def register_user(
     data: UserRequestAdd,
 ):
-    hashed_password = pwd_context.hash(data.password)
+    hashed_password = AuthService().hash_password(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
-        try:
-            await UsersRepository(session).add(new_user_data)
-            await session.commit()
-        except Exception as e:
-            raise (f"ошибка {e}")
-        else:
-            return {"status": "OK"}
+        await UsersRepository(session).add(new_user_data)
+        await session.commit()
+
+    return {"status": "OK"}
 
 
 @router.post("/login")
@@ -51,10 +31,29 @@ async def login_user(
         user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
         if not user:
             raise HTTPException(status_code=401, detail="Пользователь с таким from email не зарегистрирован")
-        if not verify_password(data.password, user.hashed_password):
+        if not AuthService().verify_password(data.password, user.hashed_password):
             raise HTTPException(status_code=401, detail="Пароль неверный")
-
-
-        access_token = create_access_token({"user_id": user.id})
+        access_token = AuthService().create_access_token({"user_id": user.id})
         response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
+
+@router.get("/auth_only")
+async def auth_only(
+        request: Request,
+        authorization: str = Header(None)
+):
+    # Попробуем получить токен из заголовка Authorization
+    if authorization:
+        # Обычно токен передается в формате "Bearer <token>"
+        token = authorization.split(' ')[1] if 'Bearer ' in authorization else authorization
+        return JSONResponse(content={"access_token": token})
+
+    # Если токен не найден в заголовках, проверим тело запроса
+    body = await request.json()
+    if 'access_token' in body:
+        return JSONResponse(content={"access_token": body['access_token']})
+
+    # Если токен не найден ни в заголовках, ни в теле запроса
+    raise HTTPException(status_code=400, detail="Access token not found in the request.")
+
+
